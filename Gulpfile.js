81f34a6f15f8ddc455f385c0cf618a9ce3cbc67c
Armin Maurice <armin@conscia.co>
2017-03-29 03:23:31 -0400

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gulpLoadPlugins = require('gulp-load-plugins');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
+
